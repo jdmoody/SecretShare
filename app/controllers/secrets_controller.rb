2e7849129a1706c996865352a0751b3f8841a0f1
jdmoody <moodyofjonathan@gmail.com>
2014-03-07 10:49:44 -0800

    initial commit

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,23 @@
+class SecretsController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    @user = User.find(params[:secret][:recipient_id])
+    @secret = @user.received_secrets.new(secret_params)
+    @secret.author_id = current_user.id
+
+    if @secret.save
+      redirect_to user_url(@user)
+    else
+      flash[:errors] = @secret.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+  def secret_params
+    params.require(:secret).permit(:title)
+  end
+end
