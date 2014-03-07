ad1b2a156344fead72c7531258af1def17bb4aab
jdmoody <moodyofjonathan@gmail.com>
2014-03-07 12:06:24 -0800

    Phase III: Remote friendships

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,12 +1,14 @@
 class FriendshipsController < ApplicationController
   def create
+    sleep(2)
     @out_friend_id = current_user.id
     @in_friend_id = params[:user_id]
     @friendship = Friendship.new(out_friend_id: @out_friend_id,
                                  in_friend_id: @in_friend_id)
 
     if @friendship.save
-      redirect_to users_url
+      render :json => @friendship
+      # head :ok?????
     else
       flash[:errors] = @friendship.errors.full_messages
       redirect_to users_url
